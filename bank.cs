57ed559ecd6833231b88b5fc55ffc7301e7afb21
Jon <jashlayug@gmail.com>
2024-05-07 19:25:53 -0600

    bank.cs has been added

---
+++ bank.cs
@@ -0,0 +1,7 @@
+public class  Bank{
+	public int bankstatmentId {get; set;}
+	public string AccountHolder {get; set;}
+	public int AccountNumber {get; set;}
+	public int Age {get; set;}
+	public int SecurityCode {get; set;}
+	public bool Transactions  {get; set;}
